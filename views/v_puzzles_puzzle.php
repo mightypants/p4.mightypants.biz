1aafc9c11d601f18dabe568eb240443b3dbed392
Jonnie Dredge <dredge@g.harvard.edu>
2013-12-22 15:03:24 -0500

    cleaned up go back  and 'loading' message position

--- views/v_puzzles_puzzle.php
+++ views/v_puzzles_puzzle.php
@@ -1,4 +1,4 @@
-<a href="/" class="confirmNavigate">Go Back</a>
+<a href="/" class="returnHome">Go Back</a>
 <div id="puzzle"><?= $puzzle_cells; ?>
 	<div id="hidePuzzle"><p id="puzzleMsg">Start</p></div>
 	<div id="winMsg">PUZZLE SOLUTION'D!</div>
