c7b0a8f4da74bd702af574c06878f6db35436b69
Jonnie Dredge <dredge@g.harvard.edu>
2013-12-22 00:13:03 -0500

    fixed html validation errors

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,11 +1,11 @@
 <h2>Welcome to jDoku</h2>
 <p>It's like sudoku, but with a "j." The rules are simple: each row, column, and 3x3 box must contain each of the numbers 1-9 exactly once.  Every puzzle has only one correct solution. Log in to track your puzzle solving stats, or just select a difficulty level and start solving now.  If you don't have an account, <a class="ajaxLink" href="/users/signup">sign up here</a>.</p>
-<img src="/images/example_puzzle.png" id="examplePuzzle" />
+<img src="/images/example_puzzle.png" id="examplePuzzle" alt="sudoku example" />
 
 <div id="newGame">
 	<input type="button" class="submitBtn" value="Start Puzzle" id="startPuzzle" />
-	<input type="button" class="difficultySelect" id="easy" value="Easy"></input>
-	<input type="button" class="difficultySelect" id="medium" value="Medium"></input>
-	<input type="button" class="difficultySelect" id="hard" value="Hard"></input>
-	<input type="button" class="difficultySelect" id="vhard" value="Run Away Screaming"></input>
+	<input type="button" class="difficultySelect" id="easy" value="Easy" />
+	<input type="button" class="difficultySelect" id="medium" value="Medium" />
+	<input type="button" class="difficultySelect" id="hard" value="Hard" />
+	<input type="button" class="difficultySelect" id="vhard" value="Run Away Screaming" />
 </div>
